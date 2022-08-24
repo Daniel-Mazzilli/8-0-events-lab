cbab3529f0fe9006bd1a607bdb0e046c58fc03e9
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-08-24 02:24:21 -0400

    added buttons

--- main.js
+++ main.js
@@ -8,10 +8,12 @@ for (let i = 0; i < 100; i++) {
 
 // You may write your code here!
 
-// node list of the colors in the palette
+// node list of colors in palette
 const colorList = document.querySelectorAll(`.color`);
-// console.log(colorList)
+// currentColor
 const currentColor = document.querySelector(`#current-color`);
+// palette
+const palette = document.querySelector(`#palette`);
 
 // add event listener to each colorItem, update the currentColor once a colorItem is clicked
 colorList.forEach((colorItem) => {
@@ -20,9 +22,15 @@ colorList.forEach((colorItem) => {
   });
 });
 
+// alternate way to update currentColor
+// palette.addEventListener(`click`, (el) => {
+//   if(el.target.className === `color`){
+//     currentColor.style.backgroundColor = el.target.style.backgroundColor
+//   }
+// })
+
 // node list for cells in grid
 const gridCells = document.querySelectorAll(`#canvas .cell`);
-// console.log(gridCells)
 
 // add event listener to each cell, update grid backgroundColor by clicking on it
 gridCells.forEach((cell) => {
@@ -34,3 +42,34 @@ gridCells.forEach((cell) => {
     }
   });
 });
+
+// button div
+const buttonDiv = document.createElement(`div`)
+buttonDiv.setAttribute(`class`, `buttons`)
+currentColor.before(buttonDiv)
+buttonDiv.style.display = `grid`
+buttonDiv.style.gridTemplateColumns = `5fr 1fr 1fr 5fr`
+buttonDiv.style.columnGap = `60px`
+buttonDiv.style.marginTop = `30px`
+
+// reset button
+const reset = document.createElement(`button`);
+buttonDiv.append(reset);
+reset.textContent = `RESET`;
+reset.style.gridColumn = `2`
+reset.style.height = `40px`
+reset.addEventListener(`click`, () => {
+  gridCells.forEach((cell) => {
+    cell.style.backgroundColor = `white`;
+  });
+});
+
+// fill-all button
+const fillAll = document.createElement(`button`)
+buttonDiv.append(fillAll)
+fillAll.textContent = `FILL-ALL`
+fillAll.addEventListener(`click`, () => {
+  gridCells.forEach((cell) => {
+    cell.style.backgroundColor = currentColor.style.backgroundColor
+  })
+})
